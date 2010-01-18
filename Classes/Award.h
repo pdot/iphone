d108f98b62f9010c4f89b04179715c88b8a91055
Your Name <admin@pdot.ca>
2010-01-18 14:10:18 -0800

    Added league create and edit to the rails code.

--- Classes/Award.h
+++ Classes/Award.h
@@ -1,19 +1,18 @@
 #import "ObjectiveResource.h"
 
 @interface Award : NSObject {
+	NSString *awardId;
 	NSString *name;
 	NSString *description;
-	NSString *awardId;
 	NSDate   *updatedAt;
 	NSDate   *createdAt;
-  
 }
 
-@property (nonatomic , retain) NSDate * createdAt;
-@property (nonatomic , retain) NSDate * updatedAt;
-@property (nonatomic , retain) NSString  *awardId;
+@property (nonatomic , retain) NSString *awardId;
 @property (nonatomic , retain) NSString *name;
 @property (nonatomic , retain) NSString *description;
+@property (nonatomic , retain) NSDate   *createdAt;
+@property (nonatomic , retain) NSDate   *updatedAt;
 
 -(NSArray *) findAllNominees;
 -(NSArray *) findAllNomineesWithResponse:(NSError **)aError;
--- Classes/AwardsViewController.m
+++ Classes/AwardsViewController.m
@@ -12,6 +12,15 @@
 @implementation AwardsViewController
 @synthesize awards , tableView;
 
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
+        // Custom initialization
+		self.title = @"Categories";
+    }
+    return self;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
 	[self loadAwards];
 }
